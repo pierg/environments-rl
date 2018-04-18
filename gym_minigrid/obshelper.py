01c5eb2e019f3cd9d637ba0cb55028e6e98acec3
Piergiuseppe Mallozzi <piergiuseppe.mallozzi@gmail.com>
2018-04-18 09:30:42 +0200

    minor

--- gym_minigrid/obshelper.py
+++ gym_minigrid/obshelper.py
@@ -7,19 +7,19 @@ class ObsHelper():
     @staticmethod
     def is_facing_water(obs):
         # Use the agent observations to determine if there is water in the first tile in front of the agent
-        # obs = obs
+        # for testing we return true/false according to the string passed as parameter
         return obs == 'facing_water'
 
     @staticmethod
     def is_near_water(obs):
         # Use the agent observations to determine if there is water in from the second
         # tile on around its observation
-        # obs = obs
+        # for testing we return true/false according to the string passed as parameter
         return obs == 'near_water'
 
     @staticmethod
     def is_inside_water(obs):
-        # obs = obs
+        # for testing we return true/false according to the string passed as parameter
         return obs == 'inside_water'
 
     @staticmethod
