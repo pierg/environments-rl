ba16f9b29376c8e5c77489785836ee48fa7c64b8
Philip Crockett <philipmayoo@gmail.com>
2018-04-19 11:10:54 +0200

    Comments and formatting

--- gym_minigrid/obshelper.py
+++ gym_minigrid/obshelper.py
@@ -10,14 +10,19 @@ class ObsHelper():
     def is_unsafe_approach(observation, view_size, unsafe, in_front_of=True, ahead=2):
         """ Returns True or False if the tile ahead is an unsafe tile for the agent
 
-            observation: An observation grid to investigate
-            view_size: The agents view size
-            unsafe: A type that is deemed as unsafe for the agent
-            in_front_of: Check if the tile in front of the agent, default value is true, false if you want to check a tile further away
-            ahead: Number of tiles ahead of the agent, default value is 2, which is the next tile in front of the agent"""
+            :param observation: An observation grid to investigate
+            :param view_size: The agents view size
+            :param unsafe: A type that is deemed as unsafe for the agent
+            :param in_front_of: Check if the tile in front of the agent, default value is true, false if you want to check a tile further away
+            :param ahead: Number of tiles ahead of the agent, default value is 2, which is the next tile in front of the agent
+            :return: True / False"""
         if in_front_of:
             return isinstance(observation.get((math.floor(view_size / ahead)), view_size - ahead), unsafe)
         return isinstance(observation.get((math.floor(view_size / ahead)), view_size - ahead), unsafe)
+
+    @staticmethod
+    def is_water_in_front_of_agent(observation, view_size):
+        return ObsHelper.is_unsafe_approach(observation, view_size, Water)
 
     @staticmethod
     def is_immediate_to_worldobj(object_type):
@@ -40,10 +45,6 @@ class ObsHelper():
         """
         # 12 cases
         return True
-
-    @staticmethod
-    def is_water_in_front_of_agent(observation, view_size):
-        return ObsHelper.is_unsafe_approach(observation, view_size, Water)
 
     def is_facing_water(obs):
         # Use the agent observations to determine if there is water in the first tile in front of the agent
