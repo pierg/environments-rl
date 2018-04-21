8294b67e8db625c2387e54107177278a9ce0220a
Piergiuseppe Mallozzi <piergiuseppe.mallozzi@gmail.com>
2018-04-21 10:29:30 +0200

    refactoring of obs helper and evelope blocker

--- gym_minigrid/envelopes.py
+++ gym_minigrid/envelopes.py
@@ -1,22 +1,8 @@
-import math
-import operator
-import time
 
 import collections
-from minigrid import  AGENT_VIEW_SIZE
-from functools import reduce
 from obshelper import ObsHelper
-import numpy as np
 
-import gym
-from gym import error, spaces, utils
-
-
-import os
-
-
-import json
-from collections import namedtuple
+from helpers import config_grabber as cg
 
 from minigrid import *
 
@@ -36,12 +22,8 @@ class SafetyEnvelope(gym.core.RewardWrapper):
     def __init__(self, env, reset_on_catastrophe=False):
         super().__init__(env)
 
-        # Assumption: baby-ai-game repo folder is located in the same folder containing gym-minigrid repo folder
-        config_file_path = os.path.abspath(__file__ + "/../../../" + "/baby-ai-game/configurations/main.json")
-        with open(config_file_path, 'r') as jsondata:
-            configdata = jsondata.read()
-            self.config = json.loads(configdata,
-                                     object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+        # Grab configuration
+        self.config = cg.Configuration.grab()
 
         # Stores history of the last N observation / proposed_actions
         self.proposed_history = collections.deque(N*[(None, None)], N)
@@ -60,42 +42,39 @@ class SafetyEnvelope(gym.core.RewardWrapper):
             self.env.render('human')
 
         proposed_action = action
-
-        for i in current_obs.grid:
-            if i is not None:
-                print("WAIT!")
-
         self.proposed_history.append((current_obs, proposed_action))
 
         safe_action = proposed_action
 
-        if proposed_action == MiniGridEnv.Actions.forward:
-            if self.blocker(current_obs):
-                if self.reset_on_catastrophe:
-                    obs = self.env.gen_obs()
-                    reward = NEGATIVE_REWARD_CATASTROPHE
-                    done = True
-                    info = {'catastrophe': 1}
-                else:
-                    safe_action = MiniGridEnv.Actions.wait
-                    obs, reward, done, info = self.env.step(safe_action)
-                    reward = NEGATIVE_REWARD_CATASTROPHE
-                    info = {'catastrophe': 1}
+        if self.blocker(current_obs, proposed_action):
+            if self.reset_on_catastrophe:
+                obs = self.env.gen_obs()
+                reward = NEGATIVE_REWARD_CATASTROPHE
+                done = True
+                info = {'catastrophe': 1}
             else:
+                safe_action = MiniGridEnv.Actions.wait
                 obs, reward, done, info = self.env.step(safe_action)
+                reward = NEGATIVE_REWARD_CATASTROPHE
+                info = {'catastrophe': 1}
         else:
             obs, reward, done, info = self.env.step(safe_action)
+
+
 
         self.actual_history.append((current_obs, safe_action))
 
         # Apply the agent action to the environment or a safety action
-
         mod_reward = reward
 
 
         return obs, mod_reward, done, info
 
-    def blocker(self, observation):
-        return ObsHelper.testObs(observation, AGENT_VIEW_SIZE, Water)
+    def blocker(self, observation, action):
+        # Specifies all the cases when the blocker has to be triggered..
+
+        # Block if the agent is in front of the water and want to go forward
+        return ObsHelper.is_ahead_of_worldobj(observation, Water, 1) \
+               and action == MiniGridEnv.Actions.forward
 
 
--- gym_minigrid/obshelper.py
+++ gym_minigrid/obshelper.py
@@ -1,6 +1,6 @@
 import math
 from minigrid import AGENT_VIEW_SIZE
-from minigrid import  Water
+from minigrid import Water
 
 AGENT_GRID_LOCATION = 2
 
@@ -10,49 +10,57 @@ AGENT_GRID_LOCATION = 2
 class ObsHelper():
 
 
-    """ This is a helper method,
-        classes to check for must be specified in a function,
-        see is_water_inf_front_of_agent """
-    @staticmethod
-    def is_unsafe_in_front_of_agent(observation, view_size, unsafe):
-        """ Returns True or False if the tile ahead is an unsafe tile for the agent
-            :param observation: An observation grid to investigate
-            :param view_size: The agents view size
-            :param unsafe: A type that is deemed as unsafe for the agent
-            :return: True / False"""
-        x = (math.floor(view_size / AGENT_GRID_LOCATION))
-        y = view_size - AGENT_GRID_LOCATION
-        type = observation.get(x, y)
-        return isinstance(type, unsafe)
+    # """ This is a helper method,
+    #     classes to check for must be specified in a function,
+    #     see is_water_inf_front_of_agent """
+    # @staticmethod
+    # def is_unsafe_in_front_of_agent(observation, view_size, unsafe):
+    #     """ Returns True or False if the tile ahead is an unsafe tile for the agent
+    #         :param observation: An observation grid to investigate
+    #         :param view_size: The agents view size
+    #         :param unsafe: A type that is deemed as unsafe for the agent
+    #         :return: True / False"""
+    #     x = (math.floor(view_size / AGENT_GRID_LOCATION))
+    #     y = view_size - AGENT_GRID_LOCATION
+    #     type = observation.get(x, y)
+    #     return isinstance(type, unsafe)
+    #
+    # """ Use this to check for water in front of the agent"""
+    # @staticmethod
+    # def is_water_in_front_of_agent(observation, view_size):
+    #     return ObsHelper.is_unsafe_in_front_of_agent(observation, view_size, Water)
+    #
+    # @staticmethod
+    # def testObs(observation, view_size, unsafe, in_front_of=True, ahead=2):
+    #     x = math.floor(view_size / ahead)
+    #     y = view_size - ahead
+    #     type = observation.get(x, y)
+    #     result = isinstance(type, unsafe)
+    #     if result:
+    #         return True
+    #     else:
+    #         return False
 
-    """ Use this to check for water in front of the agent"""
-    @staticmethod
-    def is_water_in_front_of_agent(observation, view_size):
-        return ObsHelper.is_unsafe_in_front_of_agent(observation, view_size, Water)
 
     @staticmethod
-    def testObs(observation, view_size, unsafe, in_front_of=True, ahead=2):
-        x = math.floor(view_size / ahead)
-        y = view_size - ahead
-        type = observation.get(x, y)
-        result = isinstance(type, unsafe)
-        if result:
-            return True
-        else:
-            return False
-
-
-    @staticmethod
-    def is_ahead_of_worldobj(obs, object_type, distance):
+    def is_ahead_of_worldobj(obs, object_type, distance=1):
         """
         Return True if "distance" cell in front of the agent contain is of type 'object_type'
-        :param obs:
-        :param object_type:
+        :param obs: An observation grid to investigate
+        :param object_type: A type that is deemed as unsafe for the agent
         :param distance: number of cells in front (1 = the one next to the agent cell)
         :return:
         """
-
-        raise NotImplementedError
+        x = (math.floor(AGENT_VIEW_SIZE / AGENT_GRID_LOCATION))
+        y = AGENT_VIEW_SIZE - AGENT_GRID_LOCATION
+        type = obs.get(x, y)
+        # For debugging:
+        # result = isinstance(type, object_type)
+        # if result:
+        #     return True
+        # else:
+        #     return False
+        return isinstance(type, object_type)
 
 
     @staticmethod
@@ -93,7 +101,8 @@ class ObsHelper():
         :param object_type: type of WorldObj
         :return: True / False
         """
-        # 12 cases
+        # 12 possible cells around the worldobj
+        if ObsHelper.is_ahead_of_worldobj(obs, object_type, 1): return True
         if ObsHelper.is_ahead_of_worldobj(obs, object_type, 2): return True
         if ObsHelper.is_worldobj_to_left(obs, object_type): return True
         if ObsHelper.is_worldobj_to_right(obs, object_type): return True
