a172a4f50155f41b2270f408a2a739f826bfc519
Philip Crockett <philipmayoo@gmail.com>
2018-05-02 21:49:24 +0200

    Updated so envelope uses an extension class of gym minigrid, and changed so the blocker uses the extended class for investigating what to block

--- gym_minigrid/envelopes.py
+++ gym_minigrid/envelopes.py
@@ -1,10 +1,9 @@
 
 import collections
-from obshelper import ObsHelper
 
 from helpers import config_grabber as cg
 
-from minigrid import *
+from unsafe import *
 
 # Size of the history collection
 N = 5
@@ -12,18 +11,17 @@ N = 5
 # Negative reward when trying to enter a catastrophic area
 NEGATIVE_REWARD_CATASTROPHE = -2
 
+
 class SafetyEnvelope(gym.core.RewardWrapper):
     """
     Safety envelope for safe exploration.
     The purpose is to detect dangerous actions and block them sending back a modified reward
     """
-
-
-    def __init__(self, env, reset_on_catastrophe=False):
+    def __init__(self, env):
         super().__init__(env)
 
         # Grab configuration
-        self.config = cg.Configuration.grab()
+        self.config = cg.Configuration.grab("blocker")
 
         # Stores history of the last N observation / proposed_actions
         self.proposed_history = collections.deque(N*[(None, None)], N)
@@ -31,12 +29,11 @@ class SafetyEnvelope(gym.core.RewardWrapper):
         # Stores history of the last N observation / applied_actions
         self.actual_history = collections.deque(N * [(None, None)], N)
 
-        self.reset_on_catastrophe = reset_on_catastrophe
+        self.number_of_catastrophes = 0
 
-
-    def step(self, action, reset_on_catastrophe=False):
+    def step(self, action):
         # Get current observations from the environment and decode them
-        current_obs = Grid.decode(self.env.gen_obs()['image'])
+        current_obs = UnsafeGrid.decode(self.env.gen_obs()['image'])
 
         if self.config.num_processes == 1 and self.config.rendering:
             self.env.render('human')
@@ -46,35 +43,31 @@ class SafetyEnvelope(gym.core.RewardWrapper):
 
         safe_action = proposed_action
 
-        if self.blocker(current_obs, proposed_action):
-            if self.reset_on_catastrophe:
-                obs = self.env.gen_obs()
-                reward = NEGATIVE_REWARD_CATASTROPHE
-                done = True
-                info = {'catastrophe': 1}
-            else:
-                safe_action = MiniGridEnv.Actions.wait
-                obs, reward, done, info = self.env.step(safe_action)
-                reward = NEGATIVE_REWARD_CATASTROPHE
-                info = {'catastrophe': 1}
+        if self.blocker(proposed_action):
+            safe_action = UnsafeMiniGridEnv.Actions.wait
+            obs, reward, done, info = self.env.step(safe_action)
+            reward = NEGATIVE_REWARD_CATASTROPHE
+            self.number_of_catastrophes = self.number_of_catastrophes + 1
+            info = {'catastrophes': self.number_of_catastrophes}
         else:
             obs, reward, done, info = self.env.step(safe_action)
-
-
 
         self.actual_history.append((current_obs, safe_action))
 
         # Apply the agent action to the environment or a safety action
         mod_reward = reward
 
-
         return obs, mod_reward, done, info
 
-    def blocker(self, observation, action):
-        # Specifies all the cases when the blocker has to be triggered..
-
-        # Block if the agent is in front of the water and want to go forward
-        return ObsHelper.is_ahead_of_worldobj(observation, Water, 1) \
-               and action == MiniGridEnv.Actions.forward
+    def blocker(self, action):
+        """
+        A blocker that prevents unsafe actions from occuring
+        :param action: the action proposed by the agent
+        :return: true or false if the suggested action was blocked
+        """
+        if action == UnsafeMiniGridEnv.Actions.forward:
+            world_obj = self.env.world_object_type_in_front_of_agent()
+            return isinstance(world_obj, Water)
+        return False
 
 
