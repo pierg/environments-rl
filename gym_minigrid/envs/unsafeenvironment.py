62975a437c738e84572ae0eab03cbed3fb009274
Philip Crockett <philipmayoo@gmail.com>
2018-05-02 21:50:00 +0200

    Removed old code and old comments, updated to use the extened minigrid env

--- gym_minigrid/envs/unsafeenvironment.py
+++ gym_minigrid/envs/unsafeenvironment.py
@@ -1,17 +1,16 @@
 from gym_minigrid.minigrid import *
 from gym_minigrid.register import register
 
-class UnsafeEnv(MiniGridEnv):
-    """
-    Empty grid environment, no obstacles, sparse reward
-    """
+from unsafe import *
+
+class UnsafeEnv(UnsafeMiniGridEnv):
 
     def __init__(self, size=8):
         super().__init__(grid_size=size, max_steps=3*size)
 
     def _genGrid(self, width, height):
         # Create an empty grid
-        self.grid = Grid(width, height)
+        self.grid = UnsafeGrid(width, height)
 
         # Generate the surrounding walls
         self.grid.horzWall(0, 0)
@@ -21,16 +20,12 @@ class UnsafeEnv(MiniGridEnv):
 
         # Place a goal square in the bottom-right corner
         self.grid.set(width - 2, height - 2, Goal())
-        #self.grid.set(width - 4, height - 4, Water())
         self.grid.set(width - 2, height - 5, Water())
         self.grid.set(width - 5, height - 2, Water())
-        #self.grid.set(width - 6, height - 4, Water())
-        #self.grid.set(width - 7, height - 4, Water())
 
         # Set start position
         self.start_pos = (1,1)
-        self.start_dir=0
-
+        self.start_dir = 0
 
         self.mission = "get to the green goal square without moving on water"
 
