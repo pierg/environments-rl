5d85f1095baa3c3c4e2e264bb8c7711afc2bf5c3
ardba <gusdebana@student.gu.se>
2018-05-22 16:52:01 +0200

    Getting rid of turnaround

--- gym_minigrid/action_planning/action.py
+++ gym_minigrid/action_planning/action.py
@@ -78,32 +78,32 @@ class Action:
                                       (StateEnum.orientation_east, True))
                 self.effects = ((StateEnum.orientation_south, True), (StateEnum.orientation_east, False))
         
-        elif name == ExMiniGridEnv.Actions.turnaround:
-            self.cost = 2
-            if orientation == StateEnum.orientation_north:
-                self.preconditions = (
-                    (StateEnum.south_is_clear, True),
-                    (StateEnum.south_is_safe, True),
-                    (StateEnum.orientation_north, True))
-                self.effects = ((StateEnum.orientation_south, True), (StateEnum.orientation_north, False))
-            elif orientation == StateEnum.orientation_south:
-                self.preconditions = (
-                    (StateEnum.north_is_clear, True),
-                    (StateEnum.north_is_safe, True),
-                    (StateEnum.orientation_south, True)
-                )
-                self.effects = ((StateEnum.orientation_north, True), (StateEnum.orientation_south, False))
-            elif orientation == StateEnum.orientation_west:
-                self.preconditions = (
-                    (StateEnum.east_is_clear, True),
-                    (StateEnum.east_is_safe, True),
-                    (StateEnum.orientation_west, True))
-                self.effects = ((StateEnum.orientation_east, True), (StateEnum.orientation_west, False))
-            elif orientation == StateEnum.orientation_east:
-                self.preconditions = ((StateEnum.west_is_clear, True),
-                                      (StateEnum.west_is_safe, True),
-                                      (StateEnum.orientation_east, True))
-                self.effects = ((StateEnum.orientation_west, True), (StateEnum.orientation_east, False))
+        # elif name == ExMiniGridEnv.Actions.turnaround:
+        #     self.cost = 2
+        #     if orientation == StateEnum.orientation_north:
+        #         self.preconditions = (
+        #             (StateEnum.south_is_clear, True),
+        #             (StateEnum.south_is_safe, True),
+        #             (StateEnum.orientation_north, True))
+        #         self.effects = ((StateEnum.orientation_south, True), (StateEnum.orientation_north, False))
+        #     elif orientation == StateEnum.orientation_south:
+        #         self.preconditions = (
+        #             (StateEnum.north_is_clear, True),
+        #             (StateEnum.north_is_safe, True),
+        #             (StateEnum.orientation_south, True)
+        #         )
+        #         self.effects = ((StateEnum.orientation_north, True), (StateEnum.orientation_south, False))
+        #     elif orientation == StateEnum.orientation_west:
+        #         self.preconditions = (
+        #             (StateEnum.east_is_clear, True),
+        #             (StateEnum.east_is_safe, True),
+        #             (StateEnum.orientation_west, True))
+        #         self.effects = ((StateEnum.orientation_east, True), (StateEnum.orientation_west, False))
+        #     elif orientation == StateEnum.orientation_east:
+        #         self.preconditions = ((StateEnum.west_is_clear, True),
+        #                               (StateEnum.west_is_safe, True),
+        #                               (StateEnum.orientation_east, True))
+        #         self.effects = ((StateEnum.orientation_west, True), (StateEnum.orientation_east, False))
 
     Action = TypeVar('Action')
 
