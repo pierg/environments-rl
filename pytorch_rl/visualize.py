5de5130450eef81e9d58477e02e1f40c1345a8f6
Piergiuseppe Mallozzi <piergiuseppe.mallozzi@gmail.com>
2018-07-30 16:40:23 +0900

    added median reward to visdom plot

--- pytorch_rl/visualize.py
+++ pytorch_rl/visualize.py
@@ -2,12 +2,14 @@ import numpy as np
 
 vis = None
 
-win = None
+win1 = None
+win2 = None
 
 avg_reward = 0
 
 X = []
-Y = []
+Y1 = []
+Y2 = []
 
 
 def test_visdom():
@@ -40,7 +42,8 @@ def visdom_plot(
     from visdom import Visdom
 
     global vis
-    global win
+    global win1
+    global win2
     global avg_reward
 
     if vis is None:
@@ -54,12 +57,13 @@ def visdom_plot(
     avg_reward = avg_reward * 0.9 + 0.1 * mean_reward
 
     X.append(total_num_steps)
-    Y.append(avg_reward)
+    Y1.append(avg_reward)
+    Y2.append(mean_reward)
 
     # The plot with the handle 'win' is updated each time this is called
-    win = vis.line(
+    win1 = vis.line(
         X = np.array(X),
-        Y = np.array(Y),
+        Y = np.array(Y1),
         opts = dict(
             #title = 'All Environments',
             xlabel='Total time steps',
@@ -72,7 +76,26 @@ def visdom_plot(
             width=900,
             height=500
         ),
-        win = win
+        win = win1
+    )
+
+    # The plot with the handle 'win' is updated each time this is called
+    win2 = vis.line(
+        X=np.array(X),
+        Y=np.array(Y2),
+        opts=dict(
+            # title = 'All Environments',
+            xlabel='Total time steps',
+            ylabel='Mean Reward',
+            ytickmin=0,
+            # ytickmax=1,
+            # ytickstep=0.1,
+            # legend=legend,
+            # showlegend=True,
+            width=900,
+            height=500
+        ),
+        win=win2
     )
 
 
