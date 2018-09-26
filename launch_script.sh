6c52bdb6c16e0d4e6743c9cd295785ddc000898f
ezecastellano <ecastellano@nii.ac.jp>
2018-09-26 14:39:40 +0200

    Allowing multiple iterations on the same random environment

--- launch_script.sh
+++ launch_script.sh
@@ -12,13 +12,14 @@ launch_monitor=0
 launch_without=0
 logstdfile=0
 
-while getopts qrt:e:w:s:i:labf opt; do
+while getopts qt:r:e:w:s:i:labf opt; do
     case ${opt} in
         q)
             qlearning=1
             ;;
         r)
             random=1
+            random_iterations=${OPTARG}
             start_training=1
             ;;
         t)
@@ -57,12 +58,18 @@ while getopts qrt:e:w:s:i:labf opt; do
     esac
 done
 shift $((OPTIND -1))
-echo "iterations :$iterations"
 i=0
 if ! [ $iterations ]; then
     iterations=1
 fi
-while [ $iterations -ne $i ]; do
+echo "iterations :$iterations"
+
+if ! [ $random_iterations ]; then
+    random_iterations=1
+fi
+echo "random iterations :$random_iterations"
+
+while [ $random_iterations -ne $i ]; do
     if [ $random ]; then
         if [ $light ]; then
             echo "...creating a random light environment... using environment_file: $environment and reward_file: $reward"
@@ -73,93 +80,95 @@ while [ $iterations -ne $i ]; do
         fi
         configuration_file="randoms/$configuration_file"
     fi
+    j=0
+    while [ $iterations -ne $j ]; do
+        echo "...environment name is..."
+        echo $configuration_file
 
-    echo "...environment name is..."
-    echo $configuration_file
+        if [ $configuration_file -eq "main.json" ]; then
+            echo "using default configuration file: $configuration_file"
+            cd ./configurations
+        else
+            echo "...updating selected configuration file..."
+            cd ./configurations
+            echo "using configuration file: $configuration_file"
+            yes | cp -rf $configuration_file "main.json"
+        fi
 
-    if [ $configuration_file -eq "main.json" ]; then
-        echo "using default configuration file: $configuration_file"
-        cd ./configurations
-    else
-        echo "...updating selected configuration file..."
-        cd ./configurations
-        echo "using configuration file: $configuration_file"
-        yes | cp -rf $configuration_file "main.json"
-    fi
+        cd ..
 
-    cd ..
+        # Use virtual environment if exists
+        if [ -d "venv" ]; then
+          echo "...activating python venv..."
+          source ./venv/bin/activate
+        fi
 
-    # Use virtual environment if exists
-    if [ -d "venv" ]; then
-      echo "...activating python venv..."
-      source ./venv/bin/activate
-    fi
+        echo "...setting up python environment..."
+        PYTHONPATH=../gym-minigrid/:../gym-minigrid/gym_minigrid/:./configurations:./:$PYTHONPATH
+        export PYTHONPATH
 
-    echo "...setting up python environment..."
-    PYTHONPATH=../gym-minigrid/:../gym-minigrid/gym_minigrid/:./configurations:./:$PYTHONPATH
-    export PYTHONPATH
+        if ! [ $stop ]; then
+            stop=0
+        fi
 
-    if ! [ $stop ]; then
-        stop=0
-    fi
+        chmod 744 ./pytorch_dqn/main.py
+        chmod 744 ./pytorch_a2c/main.py
 
-    chmod 744 ./pytorch_dqn/main.py
-    chmod 744 ./pytorch_a2c/main.py
-
-    if [ $start_training -eq 1 ] && [ $logstdfile -eq 0 ]; then
-    echo "...launching the training without logging to file..."
-            if [ $launch_monitor -eq 1 ]; then
-                echo "+++++ With Envelope +++++"
-                if [ $qlearning -eq 1 ]; then
-                    echo "launching: ./pytorch_dqn/main.py --stop $stop --record"
-                    python3 ./pytorch_dqn/main.py --stop $stop --record --norender
-                else
-                    echo "launching ./pytorch_a2c/main.py --stop $stop --record --norender"
-                    python3 ./pytorch_a2c/main.py --stop $stop --record --norender
+        if [ $start_training -eq 1 ] && [ $logstdfile -eq 0 ]; then
+        echo "...launching the training without logging to file..."
+                if [ $launch_monitor -eq 1 ]; then
+                    echo "+++++ With Envelope +++++"
+                    if [ $qlearning -eq 1 ]; then
+                        echo "launching: ./pytorch_dqn/main.py --stop $stop --record"
+                        python3 ./pytorch_dqn/main.py --stop $stop --record --norender
+                    else
+                        echo "launching ./pytorch_a2c/main.py --stop $stop --record --norender"
+                        python3 ./pytorch_a2c/main.py --stop $stop --record --norender
+                    fi
                 fi
-            fi
-            if [ $launch_without -eq 1 ]; then
-                echo "------ Without Envelope -----"
-                if [ $qlearning -eq 1 ]; then
-                    echo "launching: ./pytorch_dqn/main.py --stop $stop --record"
-                    python3 ./pytorch_dqn/main.py --stop $stop --record --norender --nomonitor
-                else
-                    echo "launching: ./pytorch_a2c/main.py --stop $stop --record --norender --nomonitor"
-                    python3 ./pytorch_a2c/main.py --stop $stop --record --norender --nomonitor
+                if [ $launch_without -eq 1 ]; then
+                    echo "------ Without Envelope -----"
+                    if [ $qlearning -eq 1 ]; then
+                        echo "launching: ./pytorch_dqn/main.py --stop $stop --record"
+                        python3 ./pytorch_dqn/main.py --stop $stop --record --norender --nomonitor
+                    else
+                        echo "launching: ./pytorch_a2c/main.py --stop $stop --record --norender --nomonitor"
+                        python3 ./pytorch_a2c/main.py --stop $stop --record --norender --nomonitor
+                    fi
                 fi
-            fi
-            echo "plotting..."
-            python3 ./evaluations/plot_dqn.py
-            python3 ./evaluations/plot_single.py
-    fi
-    if [ $start_training -eq 1 ] && [ $logstdfile -eq 1 ]; then
-    echo "...launching the training logging to file..."
-            if [ $launch_monitor -eq 1 ]; then
-                echo "+++++ With Envelope +++++"
-                if [ $qlearning -eq 1 ]; then
-                    echo "launching: ./pytorch_dqn/main.py --stop $stop --record"
-                    python3 ./pytorch_dqn/main.py --stop $stop --record --norender --logstdfile
-                else
-                    echo "launching: ./pytorch_a2c/main.py --stop $stop --record --norender --logstdfile"
-                    python3 ./pytorch_a2c/main.py --stop $stop --record --norender --logstdfile
+                echo "plotting..."
+                python3 ./evaluations/plot_dqn.py
+                python3 ./evaluations/plot_single.py
+        fi
+        if [ $start_training -eq 1 ] && [ $logstdfile -eq 1 ]; then
+        echo "...launching the training logging to file..."
+                if [ $launch_monitor -eq 1 ]; then
+                    echo "+++++ With Envelope +++++"
+                    if [ $qlearning -eq 1 ]; then
+                        echo "launching: ./pytorch_dqn/main.py --stop $stop --record"
+                        python3 ./pytorch_dqn/main.py --stop $stop --record --norender --logstdfile
+                    else
+                        echo "launching: ./pytorch_a2c/main.py --stop $stop --record --norender --logstdfile"
+                        python3 ./pytorch_a2c/main.py --stop $stop --record --norender --logstdfile
+                    fi
                 fi
-            fi
-            if [ $launch_without -eq 1 ]; then
-                echo "------ Without Envelope -----"
-                if [ $qlearning -eq 1 ]; then
-                    echo "launching: ./pytorch_dqn/main.py --stop $stop --record --norender --nomonitor --logstdfile"
-                    python3 ./pytorch_dqn/main.py --stop $stop --record --norender --nomonitor --logstdfile
-                else
-                    echo "launching: ./pytorch_a2c/main.py --stop $stop --record --norender --nomonitor --logstdfile"
-                    python3 ./pytorch_a2c/main.py --stop $stop --record --norender --nomonitor --logstdfile
+                if [ $launch_without -eq 1 ]; then
+                    echo "------ Without Envelope -----"
+                    if [ $qlearning -eq 1 ]; then
+                        echo "launching: ./pytorch_dqn/main.py --stop $stop --record --norender --nomonitor --logstdfile"
+                        python3 ./pytorch_dqn/main.py --stop $stop --record --norender --nomonitor --logstdfile
+                    else
+                        echo "launching: ./pytorch_a2c/main.py --stop $stop --record --norender --nomonitor --logstdfile"
+                        python3 ./pytorch_a2c/main.py --stop $stop --record --norender --nomonitor --logstdfile
+                    fi
                 fi
-            fi
-            echo "plotting..."
-            python3 ./evaluations/plot_dqn.py
-            python3 ./evaluations/plot_single.py
-    fi
+                echo "plotting..."
+                python3 ./evaluations/plot_dqn.py
+                python3 ./evaluations/plot_single.py
+        fi
+        let j+=1
+    done
     let "i+=1"
-
 done
 
 
